36230e9f1109a1ce76cb4adfa2d7fc6b6a53f84c
raniaw@web.de <raniaw@web.de>
2023-01-20 17:12:43 +0100

    Repository

---
+++ 02-backend/springboot-ecommerce/spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.springboot.ecommerce.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.springboot.ecommerce.entity.Product;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	// select * from product where category_id=?
+	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+	Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
+
+}
