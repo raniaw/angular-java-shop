ef225dde302fac44f72848dadf1ae93f1d5522ec
raniaw@web.de <raniaw@web.de>
2023-01-26 11:56:16 +0100

    Create Customer Repository

---
+++ 02-backend/springboot-ecommerce/spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/dao/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.ecommerce.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.ecommerce.entity.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
