1a66f18d20ef32a973eeec6773352d9eb3f798f4
raniaw@web.de <raniaw@web.de>
2023-01-27 21:11:53 +0100

    Create function findByEmail

--- 02-backend/springboot-ecommerce/spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/dao/CustomerRepository.java
+++ 02-backend/springboot-ecommerce/spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/dao/CustomerRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.springboot.ecommerce.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	Customer findByEmail(String theEmail);
 }
