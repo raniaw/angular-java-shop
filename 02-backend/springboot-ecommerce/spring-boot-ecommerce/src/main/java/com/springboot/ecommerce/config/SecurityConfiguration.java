b0100f2e0bf99ca7258e9790c48654926dbb1a32
raniaw@web.de <raniaw@web.de>
2023-01-30 15:16:02 +0100

    Backend  SecurityConfiguration with okta

---
+++ 02-backend/springboot-ecommerce/spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.springboot.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+import com.okta.spring.boot.oauth.Okta;
+
+@Configuration
+public class SecurityConfiguration {
+
+	  @Bean
+	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    	http.authorizeHttpRequests(configurer->{
+	    		configurer.requestMatchers("/api/orders/**").authenticated().requestMatchers("/**").permitAll();;
+	    		
+	    	}).oauth2ResourceServer().jwt();
+	    	
+	        // add CORS filters
+	        http.cors();
+	 
+	        // add content negotiation strategy
+	        http.setSharedObject(ContentNegotiationStrategy.class,
+	        		 new HeaderContentNegotiationStrategy());
+	         
+
+        // Send a 401 message to the browser (w/o this, you'll see a blank page)
+        Okta.configureResourceServer401ResponseBody(http);
+        return http.build();
+    }
+}
+
